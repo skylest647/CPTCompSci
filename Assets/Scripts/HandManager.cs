14dc130798efe087b4d8d810e9b4888f5d61b082
Anson <ansonchan815@gmail.com>
2026-01-21 15:29:51 -0500

    Quick bug and FINAL fix
    Changed so it sorts by suit as well

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -26,11 +26,19 @@ public class HandManager
     }
 
     public void SortHand(){
-        for (int i = 0; i < Hand.Count - 1; i++)
-        {
-            for (int j = 0; j < Hand.Count - i - 1; j++)
-            {
-                if (Hand[j].GetNumericValue() > Hand[j + 1].GetNumericValue())
+        for (int i = 0; i < Hand.Count - 1; i++){
+            for (int j = 0; j < Hand.Count - i - 1; j++){
+                bool swap = false;
+                if (Hand[j].GetNumericValue() > Hand[j + 1].GetNumericValue()){
+                    swap = true;
+                }
+                else if (Hand[j].GetNumericValue() == Hand[j + 1].GetNumericValue())
+                {
+                    if (SuitOrder(Hand[j].GetSuit()) > SuitOrder(Hand[j + 1].GetSuit())){
+                        swap = true;
+                    }
+                }
+                if (swap)
                 {
                     Card temp = Hand[j];
                     Hand[j] = Hand[j + 1];
@@ -39,16 +47,27 @@ public class HandManager
             }
         }
     }
-    public int FindCardIndex(int value, CardSuit suit){
+
+    public int FindCardIndex(int value, string suit){
         for (int i = 0; i < Hand.Count; i++)
         {
             Card card = Hand[i];
 
             if (card.GetNumericValue() == value && card.GetSuit() == suit)
             {
-                return i; 
+                return i;
             }
         }
-        return -1; 
+        return -1;
+    }
+    private int SuitOrder(string suit){
+        switch (suit)
+        {
+            case "Clubs": return 1;
+            case "Diamonds": return 2;
+            case "Hearts": return 3;
+            case "Spades": return 4;
+            default: return 0;
+        }
     }
 }
